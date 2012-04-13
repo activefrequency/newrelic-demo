0f52ddf4975fc31722120012e0ed91a27542874c
Kevin Grinberg <kevin@activefrequency.com>
2012-04-13 01:51:36 -0400

    Views

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^test-string/', 'views.test_string'),
-    (r'^test-float/', 'views.test_float'),
-    (r'^test-decimal/', 'views.test_decimal'),
+    (r'^test-string/', 'newrelicdemo.views.test_string'),
+    (r'^test-float/', 'newrelicdemo.views.test_float'),
+    (r'^test-decimal/', 'newrelicdemo.views.test_decimal'),
 )
